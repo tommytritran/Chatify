9ce69b9e395f511188e4913038a037076564a08e
Tommy Tran <tommytran@tommys-macbook-pro.local>
2018-09-27 17:59:06 -0700

    Initial Commit

---
+++ Chatify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatify
+//
+//  Created by Tommy Tran on 27/09/2018.
+//  Copyright © 2018 Tommy Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
